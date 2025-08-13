af3eefd07fd44b6449b76f60ee12ddcadde4469d
ObsidianAura <gamer13chaos@gmail.com>
2025-08-13 23:30:53 +0530

    Add files via upload

---
+++ pages/Home.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import HeroSection from '../components/HeroSection';
+import IngredientStorySection from '../components/IngredientStorySection';
+import FeaturedRitualsSection from '../components/FeaturedRitualsSection';
+import FounderStorySection from '../components/FounderStorySection';
+import BestSellersSection from '../components/BestSellersSection';
+
+export default function Home() {
+  return (
+    <main>
+      <HeroSection />
+      <IngredientStorySection />
+      <FeaturedRitualsSection />
+      <FounderStorySection />
+      <BestSellersSection />
+    </main>
+  );
+}
